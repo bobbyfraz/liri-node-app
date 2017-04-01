b99bdf3d96922b942e1c80fa1a1d3ec051a9bbae
Bobby Frazette <Bobby Frazette>
2017-04-01 12:51:01 -0400

    all files functional

--- liri.js
+++ liri.js
@@ -1,106 +1,112 @@
-var accessKeys = require("./keys.js");
-var twitterR = require('twitter');
-var spotify = require('spotify');
-var request = require("request");
 var fs = require("fs");
-//var textFile = require("./random.txt");
+var Twitter = require('twitter');
+var accessKeys = require('./keys.js');
+var spotify = require("spotify");
+var request = require("request");
+
 var command = process.argv[2];
-var record = "\n" + process.argv[2] + "," + process.argv[3]
+var argument2 = process.argv[3];
 
-function omdb(){
-	function movieInfo(){
-		request("http://www.omdbapi.com/?t="+movieTitle+"&r=json", function(error, response, body) {
-		console.log("Title: " + JSON.parse(body).Title);
-		console.log("Year: " + JSON.parse(body).Year);
-		console.log("Rating: " + JSON.parse(body).Rated);
-		console.log("Country: " + JSON.parse(body).Country);
-		console.log("Language: " + JSON.parse(body).Language);
-		console.log("Plot: " + JSON.parse(body).Plot);
-		console.log("Main Actors: " + JSON.parse(body).Actors);
-		console.log("Rotten Tomatoes rating is: " + JSON.parse(body).Ratings);
-		console.log("Rotten Tomatoes URL is: https://www.rottentomatoes.com/m/" + movieTitle);
-		});
-	};
-	var movieTitle = process.argv[3];
-	request("http://www.omdbapi.com/?t="+movieTitle+"&r=json", function(error, response, body) {
-		if (response.statusCode === 200) {
-			movieInfo();
-//			fs.appendFile("random.txt", record);
-		}
-		else if (error) {
-			movieTitle = "Mr. Nobody";
-			console.log("There has been an error processing your request.  Here is information on Mr. Nobody");
-			request("http://www.omdbapi.com/?t="+movieTitle+"&r=json", function(error, response, body) {
-					movieInfo();
+var client = new Twitter(accessKeys.twitterKeys);
+
+function spotifySong() {
+	spotify.search({ type: "track", query: argument2 }, function(err, data) {
+		if (!err) {
+			fs.appendFile("records.txt", "\n" + "NEW DATA" + "-------"+ '\n', function(err) {
+				if (err) {
+					console.log(err);
+				}
 			});
-		};
-	});
-};
 
-function spotifySongs(){
-	var songName = process.argv[3];
-	spotify.search({ type: 'track', query: songName }, function(err, data) {
-		if (err) {
+				var infoData = ["Artist: " + data.tracks.items[0].artists[0].name, "Song Name: " + data.tracks.items[0].name, 
+				"Album Name: " + data.tracks.items[0].album.name, "Preview URL: " + data.tracks.items[0].preview_url];
+				for (var i = 0; i < infoData.length; i++) {
+					console.log(infoData[i]);
+					fs.appendFile("records.txt", infoData[i] + "\n", function(err) {
+						if (err) {
+							console.log(err);
+						}
+					});
+			};
+		}
+		else {
 			console.log('Error occurred: ' + err);
 			return;
-		}
-		else {
-			console.log("Song: " + data.tracks.items[0].name);
-			console.log("Artist: " + data.tracks.items[0].artists[0].name);
-			console.log("Album: " + data.tracks.items[0].album.name);
-			console.log("Preview URL: " + "<a href=" + data.tracks.items[0].preview_url + "></a>");
-//			fs.appendFile("random.txt", record);
 		};
 	});
 };
 
-function twitter(){
-	var client = new twitterR(accessKeys.twitterKeys);
-	client.get('statuses/user_timeline.json?count=20', function(err, tweets, response) {
-		if (!err) {
+function myTweets() {
+	client.get('statuses/user_timeline.json?count=20', function(error, tweets, response) {
+		if (!error) {
+			fs.appendFile("records.txt", "\n" + "NEW DATA" + "-------" + "\n", function(err) {
+				if (err) {
+					console.log(err);
+					}
+			});
 			for (var i = 0; i < tweets.length; i++) {
-				console.log(tweets[i].text + "\n ---------------------");
+				console.log(tweets[i].text + "\n -------");
+				fs.appendFile("records.txt", tweets[i].text + "\n", function(err) {
+					 if (err) {
+						console.log(err);
+						}
+				});
 			};
-//			fs.appendFile("random.txt", record);
 		}
 		else {
-			console.log("Error please retry...");
+			throw error;
+		}
+	});
+};
+
+function movieOmdb() {
+	request("http://www.omdbapi.com/?t=" + argument2 + "&y=&plot=short&r=json", function(error, response, body) {
+		argument2 = argument2.split(' ').join('_');
+		if (!error && response.statusCode === 200) {
+			fs.appendFile("records.txt", "\n" + "NEW DATA" + "-------" + "\n", function(err) 
+			{
+				if (err) {
+					console.log(err);
+					}
+			});
+			var infoData = ["* Title: " + JSON.parse(body).Title, "* Year: " + JSON.parse(body).Year, "* IMDB Rating: " + JSON.parse(body).imdbRating,
+				"* Country: " + JSON.parse(body).Country, "* Language: " + JSON.parse(body).Language, 
+				"* Plot: " + JSON.parse(body).Plot, "* Actors: " + JSON.parse(body).Actors, "* Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value,
+				"* Rotten Tomatoes URL: https://www.rottentomatoes.com/m/" + argument2];
+
+			for (var i = 0; i < infoData.length; i++) {
+				console.log(infoData[i]);
+				fs.appendFile("records.txt", infoData[i] + "\n", function(err) {
+					if (err) {
+						console.log(err);
+					}
+				});
+			}
 		};
 	});
 };
 
-function doIt(){
-	fs.readFile("random.txt", "utf8", function(err, data){
-//		data = data.split(",");
-		if (!err) {
-			for (var i = 0; i < data.length; i++) {
-				if (command == "movie-this") {
-					omdb(data[i]);
-				};
-				if (command == "spotify-this-song") {
-					spotifySongs(data[i]);
-				};
-				if (command == "my-tweets") {
-					twitter(data[i]);
-				};
-				if (command == "do-what-it-says"){
-					doIt(data[i]);
-				};
-			};
-		};
-	});
-	 
+function app() {
+	if (command === "spotify-this-song") {
+		spotifySong();
+	};
+
+	if (command === "my-tweets") {
+		myTweets();
+	};
+
+	if (command === "movie-this") {
+		movieOmdb();
+	}
+
+	if (command === "do-what-it-says") {
+		fs.readFile("random.txt", "utf8", function(error, data){
+			var recordedData = data.split(",", 2);
+			command = recordedData[0];
+			argument2 = recordedData[1];
+			app();
+		});
+	};
 };
 
-if (command == "movie-this") {
-	omdb();
-};
-if (command == "spotify-this-song") {
-	spotifySongs();
-};
-if (command == "my-tweets") {
-	twitter();
-};
-if (command == "do-what-it-says") {
-	doIt();
-};
+app();
